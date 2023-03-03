bc90a4bc72908330aa7ccd49279a35a0ef130ee8
DESKTOP-HBJJU88\Robotics <mohawk1712@gmail.com>
2023-03-02 20:21:07 -0500

    rearranged halo drive to run the deadband before we do finesse and delinearization. Made a levelSelf method that drives up a ramp when it detects it is up on an angle. This is triggered by holding right bumper. The values for this are not calibrated.

--- Plybot-2023/src/main/java/frc/robot/commands/displayInRange.java
+++ Plybot-2023/src/main/java/frc/robot/commands/displayInRange.java
@@ -50,23 +50,12 @@ public class displayInRange extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-
-    
-m_limelight.displayInRange();
     }
-
-    
-
-
-
-
-
 
     // Called once the command ends or is interrupted.
     @Override
